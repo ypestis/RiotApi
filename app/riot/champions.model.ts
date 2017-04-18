526285c2dec3be9ad5480043eddc7823857c4f11
Cezary Kamiński <cezary.kaminski@pollub.edu.pl>
2017-04-18 15:05:55 +0200

    dodanie modeli

---
+++ app/riot/champions.model.ts
@@ -0,0 +1,11 @@
+export class championsModel {
+            championId: number;
+            championName: string;
+            championTotalGames: number;
+            championTotalWins: number;
+            championTotalDefeats: number;
+            championDeathsPerGame: number;
+            championAssistsPerGame: number;
+            championKillsPerGame: number;
+            championKda: number;
+        }
